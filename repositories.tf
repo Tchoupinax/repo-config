13222f75d7b6e38a394b6b68ff8c187fa5aa9fd7
Tchoupinax <corentinfiloche@hotmail.fr>
2024-10-13 14:09:26 +0200

    feat: configuration of the first repository

--- repositories.tf
+++ repositories.tf
@@ -1,5 +1,5 @@
 locals {
-  repositories = yamldecode(file("${path.module}/data.yaml"))
+  repositories = yamldecode(file("${path.module}/contents/data.yaml"))
 }
 
 resource "github_repository" "repositories" {
@@ -7,5 +7,55 @@ resource "github_repository" "repositories" {
 
   name        = each.key
   description = each.value.description
-  visibility  = "private"
+  topics      = each.value.topics
+
+  visibility      = "public"
+  has_issues      = true
+  has_discussions = false
+  has_projects    = false
+  has_wiki        = false
+  has_downloads   = false
+  is_template     = false
+
+  allow_merge_commit = false
+  allow_auto_merge   = true
+  allow_rebase_merge = true
+  allow_squash_merge = true
+  archive_on_destroy = true
+
+  auto_init              = true
+  delete_branch_on_merge = true
+}
+
+#resource "github_branch_protection" "this" {
+#  for_each = github_repository.repositories
+
+#  repository_id = each.value.name
+
+#  pattern          = "master"
+#  enforce_admins   = true
+#  allows_deletions = false
+
+#  #required_pull_request_reviews {
+#  #  dismiss_stale_reviews      = true
+#  #  require_code_owner_reviews = true
+#  #}
+
+#  required_status_checks {
+#    strict = true
+#  }
+#}
+
+resource "github_repository_file" "renovate_json" {
+  for_each = github_repository.repositories
+
+  repository = each.value.name
+
+  branch              = "master"
+  commit_author       = "Terraform Bot"
+  commit_email        = "terraform@corentinfiloche.xyz"
+  commit_message      = "chore: create or upgrade renovate configuration"
+  content             = file("${path.module}/contents/renovate.json")
+  file                = "renovate.json"
+  overwrite_on_create = true
 }
