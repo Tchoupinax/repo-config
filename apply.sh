6ca25cd175317e3450c46f7c3a05c94f3f00257d
Tchoupinax <corentinfiloche@hotmail.fr>
2026-01-18 18:01:21 +0100

    chore: upgrade dep and clean (#25)

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 if [ ! -d ".terraform" ]; then
   terraform init \
-    -backend-config="access_key=$(tchoupinaxctl secrets -- bash -c 'echo $SCW_ACCESS_KEY')" \
-    -backend-config="secret_key=$(tchoupinaxctl secrets -- bash -c 'echo $SCW_SECRET_KEY')" \
-    -backend-config="bucket=$(tchoupinaxctl secrets -- bash -c 'echo $SCW_BUCKET_NAME')"
+    -backend-config="access_key=$(tchoupinaxctl secrets -- bash -c 'echo $SCW_ACCESS_KEY' | tail -n 1)" \
+    -backend-config="secret_key=$(tchoupinaxctl secrets -- bash -c 'echo $SCW_SECRET_KEY' | tail -n 1)" \
+    -backend-config="bucket=$(tchoupinaxctl secrets -- bash -c 'echo $SCW_BUCKET_NAME' | tail -n 1)"
 fi
 
-GITHUB_TOKEN=$(tchoupinaxctl secrets -- bash -c 'echo $GITHUB_TOKEN') \
+GITHUB_TOKEN=$(tchoupinaxctl secrets -- bash -c 'echo $GITHUB_TOKEN' | tail -n 1) \
   terraform apply
